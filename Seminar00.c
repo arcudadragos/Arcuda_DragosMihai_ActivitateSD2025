3b1a54b32c9a215b48d81589b7392927d1778e17
arcudadragos <arcudadragos23@stud.ase.ro>
2025-03-14 19:40:45 +0200

    testare commit (citirea unui numar intreg)

--- Seminar00.c
+++ Seminar00.c
@@ -2,7 +2,12 @@
 
 int main()
 {
-	printf("Arcuda_DragosMihai_ActivitateSD2025");
+	printf("Arcuda_DragosMihai_ActivitateSD2025\n\n>Introducere numar: ");
 	
+	int n = 0;
+	scanf_s("%d", &n);
+
+	printf(">>Numar introdus: %d", n);
+
 	return 0;
 }
